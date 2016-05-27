8dc4a4c57efc3a027254b9f4b0de4592b1799f4e
Tibold Kandrai <tibold@kandrai.eu>
2016-05-27 20:38:09 +0200

    Add project files.

---
+++ src/IdentityServer4.Contrib.AspNetIdentity/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IdentityServer4.Contrib.AspNetIdentity")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36dfdd30-81d5-4f50-9846-c8370690c83f")]
